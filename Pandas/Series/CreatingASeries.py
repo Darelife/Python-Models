6f277f7284fd1351fb5f4dfc2b28c4bc98cca6b3
itzp <67017000+Darelife@users.noreply.github.com>
2022-05-14 21:57:05 +0530

    Added constant value and dictionary series

--- Pandas/Series/CreatingASeries.py
+++ Pandas/Series/CreatingASeries.py
@@ -31,3 +31,23 @@ missingValuesSeries = pd.Series([7.5, 5.4, np.NaN, -34.5], index=[x for x in ["a
 # c     NaN
 # d   -34.5
 # dtype: float64
+
+constant_value_series = pd.Series(5, index=[x for x in "abcde"])
+# print(constant_value_series)
+
+# a    5
+# b    5
+# c    5
+# d    5
+# e    5
+# dtype: int64
+
+series_from_dict = pd.Series({"a": 1, "b": 2, "c": 3, "d": 4, "e": 5})
+# print(series_from_dict)
+
+# a    1
+# b    2
+# c    3
+# d    4
+# e    5
+# dtype: int64
