2709fc1d6a90583b1110c524e6de8e10a6ddba96
itzp <67017000+Darelife@users.noreply.github.com>
2022-05-16 00:30:30 +0530

    loc,[renaming, adding, and selecting a col.], iloc

--- Pandas/Dataframe/AccessinAndModifyingDataFrame.py
+++ Pandas/Dataframe/AccessinAndModifyingDataFrame.py
@@ -35,3 +35,69 @@ dataFrame.reset_index(inplace=True) # To reset the index of the DataFrame
 # 0    Alex   20     80
 # 1     Bob   22     90
 # 2  Clarke   23     70
+
+dataFrame.loc[3] = ['May',20,80] # To add a new row with index 3
+# print(dataFrame)
+
+#      Name  Age  Marks
+# 0    Alex   20     80
+# 1     Bob   22     90
+# 2  Clarke   23     70
+# 3     May   20     80
+
+dataFrame.loc[2] = ['Clarissa',21,79] # To update the values in the row with index 2
+# print(dataFrame)
+
+#        Name  Age  Marks
+# 0      Alex   20     80
+# 1       Bob   22     90
+# 2  Clarissa   21     79
+# 3       May   20     80
+
+#? Renaming a column
+dataFrame.columns = ['Student_Name', 'Age', 'Marks']
+# OR
+# dataFrame.rename(columns={'Name':'Student_Name'}, inplace=True)
+# print(dataFrame)
+
+#   Student_Name  Age  Marks
+# 0        Alex   20     80
+# 1         Bob   22     90
+# 2    Clarissa   21     79
+# 3         May   20     80
+
+#? Adding a new column
+dataFrame['Grade'] = dataFrame['Marks']/10
+# print(dataFrame)
+
+#   Student_Name  Age  Marks  Grade
+# 0         Alex   20     80    8.0
+# 1          Bob   22     90    9.0
+# 2     Clarissa   21     79    7.9
+# 3          May   20     80    8.0
+
+#? Selecting a specific column
+# print(dataFrame['Student_Name'])
+
+# 0        Alex
+# 1         Bob
+# 2    Clarissa
+# 3         May
+# Name: Student_Name, dtype: object
+
+#? iloc
+
+# print(dataFrame)
+
+#   Student_Name  Age  Marks  Grade
+# 0         Alex   20     80    8.0
+# 1          Bob   22     90    9.0
+# 2     Clarissa   21     79    7.9
+# 3          May   20     80    8.0
+
+# print(dataFrame.iloc[1:3, 0:2]) # To select a specific row [1:3] and column [0:2]
+
+#   Student_Name  Age
+# 1          Bob   22
+# 2     Clarissa   21
+
