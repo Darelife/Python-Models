1934a8ca859c7ff407aff264bad593977ca9f6df
itzp <67017000+Darelife@users.noreply.github.com>
2022-05-15 12:53:15 +0530

    Df from list of dict and np array

--- Pandas/Dataframe/CreatingADataFrame.py
+++ Pandas/Dataframe/CreatingADataFrame.py
@@ -67,3 +67,28 @@ dict_series_dataFrame = pd.DataFrame({'Name':Name, 'English':English, 'Math':Mat
 # 0    Alex       80    90         70       70  75
 # 1     Bob       90    80         80       90  85
 # 2  Clarke       70    70         90       80  95
+
+#? Creating a DataFrame from a List of Dictionaries
+
+student = [{'Name':'Alex', 'Age':20, 'Marks':80},{'Name':'Bob', 'Age':22, 'Marks':90},{'Name':'Clarke', 'Age':23, 'Marks':70}]
+list_dict_dataFrame = pd.DataFrame(student)
+# print(list_dict_dataFrame)
+
+#      Name  Age  Marks
+# 0    Alex   20     80
+# 1     Bob   22     90
+# 2  Clarke   23     70
+
+#? Creating a DataFrame using Numpy array
+
+array = np.array([[1,2,3],[4,5,6],[7,8,9]])
+columns = ['a','b','c']
+index = ['row1','row2','row3']
+
+array_DataFrame = pd.DataFrame(array, index = index, columns = columns)
+# print(array_DataFrame)
+
+#       a  b  c
+# row1  1  2  3
+# row2  4  5  6
+# row3  7  8  9
