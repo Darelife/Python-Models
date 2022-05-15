a2c0e52052d60786e1e3bfb29c4c3d1e0bf1548d
itzp <67017000+Darelife@users.noreply.github.com>
2022-05-15 11:01:04 +0530

    DataFrame from Series

--- Pandas/Dataframe/CreatingADatabase.py
+++ Pandas/Dataframe/CreatingADatabase.py
@@ -29,3 +29,14 @@ listDataFrame = pd.DataFrame(data, columns = ["Name","Age"], index = ["Sr.No1","
 # Sr.No3  Clarke   13
 
 #? Creating a Dataframe from a Series
+
+Student_marks = pd.Series({'Alex':80,'Bob':90,'Clarke':70})
+Student_age = pd.Series({'Alex':20,'Bob':22,'Clarke':23})
+
+seriesDataFrame = pd.DataFrame({'Marks':Student_marks,'Age':Student_age})
+# print(seriesDataFrame)
+
+#         Marks  Age
+# Alex       80   20
+# Bob        90   22
+# Clarke     70   23
