3e3bbac0af1e2ec2ca6e63ad857a6591297075b7
itzp <67017000+Darelife@users.noreply.github.com>
2022-05-16 07:00:25 +0530

    fillna

--- Pandas/Dataframe/missingData_andFillingValues.py
+++ Pandas/Dataframe/missingData_andFillingValues.py
@@ -18,3 +18,14 @@ df = (df.fillna(0)) # Fill all NaN values with 0
 # 1  8   0.0  0.0
 # 2  9  10.0  0.0
 
+set = ([[2,3,4], [8], [9,10]])
+df = pd.DataFrame(set)
+df.fillna({0:-8, 1:-10, 2:-5}, inplace = True) # Fill all NaN values with -8, -10, -5 for the rows 0, 1, 2
+# print(df)
+
+#? fflil
+set = ([[2,3,4], [8], [9,10]])
+df = pd.DataFrame(set)
+df.fillna(method = 'ffill', inplace=True) # Fill all NaN values with the previous value
+# print(df)
+#? inplace just means that the changes are made in the original DataFrame
